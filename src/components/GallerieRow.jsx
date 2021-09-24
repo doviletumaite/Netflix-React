bce9e697986a3f0c3042af8941074ba14ae47fec
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-09-24 16:41:59 +0200

    up

--- src/components/GallerieRow.jsx
+++ src/components/GallerieRow.jsx
@@ -4,35 +4,44 @@ import React, { useEffect, useState } from "react";
 import { fetchMovie } from "../data";
 
 const GallerieRow = () => {
- const [movie, setMovie] = useState([])
- const [isLoading, setIsLoading] = useState(false)
-  
- useEffect(() => {
-   const movies = fetchMovie()
-   if (movies) {
-     setMovie(movie)
-     setIsLoading(false)
-   } else {
-    setIsLoading(true)
-   }
- }, [])
+  const [movie, setMovie] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    return (
-        <div className="row px-3 bg-black mt-3">
-        <div className='col-12'>
-            {/* <h2 className="text-white">{this.state.search}</h2> */}
-        </div>
-        {
-          isLoading ?   
-          <Spinner animation="grow" variant="warning" className="mx-auto my-5"/> 
-          : 
-          movie.map(mov => <div> <p> {mov.title} </p> <img src={mov.Poster} />
-            </div>
-            )
-        }
-        </div>
-    );
-  }
+  const fetchMovies = async (id) => {
+    try {
+      let movies = await fetchMovie(id);
+     
+      if (movies) {
+        setMovie(movies);
+        //  console.log("that film", movies);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
 
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <div className="row px-3 bg-black mt-3">
+      <div className="col-12">
+        {/* <h2 className="text-white">{this.state.search}</h2> */}
+      </div>
+      {isLoading ? (
+        <Spinner animation="grow" variant="warning" className="mx-auto my-5" />
+      ) : (
+        movie.slice(0,6).map((mov) => (
+          <div>
+            <MovieCard src={mov.Poster} key={mov.imdbID} title={mov.title} /> 
+            <p> {mov.title} </p> <img src={mov.Poster} />
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
 
 export default GallerieRow;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,13 +3,13 @@ import { Link, withRouter } from "react-router-dom";
 import { fetchMovie } from "../data";
 
 const MovieCard = (props) => {
-   
-    console.log("props",props)
+    console.log("card props", props)
     return (
       <Link to="/details/:movieID">
       <a className="col-2 my-3" key={props.imdbID}>
           <div className='movieCard h-100'>
               <img className='w-100' src={props.Poster}/>
+         
             </div>
       </a>
      </Link>
