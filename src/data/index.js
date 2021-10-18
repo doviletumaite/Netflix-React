f0e6b608dfc292b5fcccdb3b317c827cfc126238
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-10-18 14:01:31 +0200

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -3,10 +3,10 @@ console.log(URL)
 
 export const fetchMovie = async(imdbID) => {
  try {
-  const response = await fetch(URL,{
+  const response = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=4e6137a25c112d7a3931970b2d03b3b4`,{
       headers:{
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTYxMzdhMjVjMTEyZDdhMzkzMTk3MGIyZDAzYjNiNCIsInN1YiI6IjYxNmM3MzE2MzNlYzI2MDA0M2Y3NTM2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bbiNtpM9FU2Lydfvip0VSZy4PV-sTvkoQPCkI3Cot_Q`
-        // Content-Type: "application/json;charset=utf-8"
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTYxMzdhMjVjMTEyZDdhMzkzMTk3MGIyZDAzYjNiNCIsInN1YiI6IjYxNmM3MzE2MzNlYzI2MDA0M2Y3NTM2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bbiNtpM9FU2Lydfvip0VSZy4PV-sTvkoQPCkI3Cot_Q`,
+        'Content-Type': 'application/json'
       }
   })
   if (response.ok) {
