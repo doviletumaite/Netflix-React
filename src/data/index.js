bf03ebb859715fa768498cfc6c6c13b6a8dfacd5
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-12-27 13:19:43 +0100

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -3,7 +3,8 @@ console.log(URL)
 
 export const fetchMovie = async(imdbID) => {
  try {
-  const response = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=4e6137a25c112d7a3931970b2d03b3b4`,{
+  const response = await fetch(`
+  https://api.themoviedb.org/3/list/{list_id}?api_key=4e6137a25c112d7a3931970b2d03b3b4&language=en-US`,{
       headers:{
         Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTYxMzdhMjVjMTEyZDdhMzkzMTk3MGIyZDAzYjNiNCIsInN1YiI6IjYxNmM3MzE2MzNlYzI2MDA0M2Y3NTM2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bbiNtpM9FU2Lydfvip0VSZy4PV-sTvkoQPCkI3Cot_Q`,
         'Content-Type': 'application/json'
